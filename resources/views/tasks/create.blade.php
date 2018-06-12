6f866c5c0f23377c5acfdd41912c4e0c8e339d7d
EC2 Default User <ec2-user@ip-172-31-17-89.us-east-2.compute.internal>
2018-06-12 07:13:31 +0000

    changeoffset

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,25 @@
 
 <h1>タスク新規作成ページ</h1>
 
+    <div class="row">
+        <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6"
+
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
  
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+     <div class="form-group">
+         {!! Form::label('status', 'ステータス:') !!}
+         {!! Form::text('status', null, ['class' => 'form-control']) !!}
+     </div>
 
+    <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+    </div>
 
-        {!! Form::submit('投稿') !!}
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
+    </div>
+</div>
 
 @endsection
